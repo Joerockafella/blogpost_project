a292812055c3cf6598de62ba3686d151e37b8aaa
Joerockafella <joeshindano@outlook.com>
2019-07-09 14:10:24 +0200

    Updated Settings

--- blogpost_project/settings.py
+++ blogpost_project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's$t7n&d^j@96dk2&vd^o7z+3*hrw*h31gg)w^@!0pflrqu0#cx'
+#SECRET_KEY = 's$t7n&d^j@96dk2&vd^o7z+3*hrw*h31gg)w^@!0pflrqu0#cx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blogpost-app.herokuapp.com']
 
 
 # Application definition
